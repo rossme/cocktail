27ee3ded687b2a08c599d1a6202bcc583919d36b
Ross Buddie <r.buddie@gmail.com>
2020-10-03 12:11:48 +0100

    navbar changes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :cocktails do
     resources :doses, only: [:create, :new]
   end
-      resources :doses, only: [:destroy]
+  
+  resources :doses, only: [:destroy]
 
 end
