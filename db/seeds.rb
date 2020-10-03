5025beb184de20253a5840e3c14f71a58ecfc486
Ross Buddie <r.buddie@gmail.com>
2020-10-03 18:41:06 +0100

    Fixed adding 'doses' for Cocktails

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 require 'json'
 require 'open-uri'
 
-url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
-ingredient_serialized = open(url).read
-ingredient = JSON.parse(ingredient_serialized)
+puts "SEEDING STARTED..."
+puts "PARSING JSON COCKTAIL DATABASE..."
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+ingredients = JSON.parse(open(url).read)
 
-50.times do
-  Ingredient.create!(name: ingredient)
+puts "CREATING INGREDIENTS"
+ingredients["drinks"].each do |ingredient|
+  ing = Ingredient.create(name: ingredient["strIngredient1"])
+  puts "create #{ing.name}"
 end
+puts "SEEDING COMPLETED"
 
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
-
-file = URI.open('https://giantbomb1.cbsistatic.com/uploads/original/9/99864/2419866-nes_console_set.png')
-cocktail = Cocktail.new(title: 'Cocktail', body: "A great cocktail")
-cocktail.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
-cocktail.save
