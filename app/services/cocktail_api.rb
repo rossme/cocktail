da564ace01cfde3d1d1bd60dbd52f34ab3ed8b2f
Ross Buddie <r.buddie@gmail.com>
2020-10-15 23:49:08 +0100

    Push like a boss

--- app/services/cocktail_api.rb
+++ app/services/cocktail_api.rb
@@ -1,6 +1,5 @@
 require 'net/http'
 require 'json'
-require 'byebug'
 
 class CocktailApiService
     def self.parsing
@@ -8,17 +7,6 @@ class CocktailApiService
         url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=#{query}`
         uri = URI(url)
         response = Net::HTTP.get(uri)
-        image = JSON.parse(response)
-        
+        image = JSON.parse(response)   
     end
-    byebug
 end
-
-
-# query = 'black'
-# url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=#{query}'
-# cocktail_search = JSON.parse(open(url).read)
-# cocktail_search["drinks"].each do |image|
-# img = Cocktail.create(url: image["strDrinkThumb"])
-# end
-
